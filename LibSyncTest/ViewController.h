78fa8e8066c3896da26213b1f0dda2226e447db8
Andrew Camera <andeh@Andrews-MacBook-Pro.local>
2012-03-13 21:32:34 +1100

    Initial Commit

---
+++ LibSyncTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LibSyncTest
+//
+//  Created by Andrew Camera on 13/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ LibSyncTestTests/LibSyncTestTests.h
@@ -0,0 +1,13 @@
+//
+//  LibSyncTestTests.h
+//  LibSyncTestTests
+//
+//  Created by Andrew Camera on 13/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface LibSyncTestTests : SenTestCase
+
+@end
